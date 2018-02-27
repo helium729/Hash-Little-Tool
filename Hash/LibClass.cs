fde618f181f530414da93a4ca773ecbf2c5c0832
helium729 <helium729@outlook.com>
2018-02-27 15:11:32 +0800

    Initial

---
+++ Hash/LibClass.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Hash
+{
+    internal enum mode { md5, sha1, sha256, sha384, sha512 };
+
+    internal class Hasher
+    {
+        private mode mode;
+
+        internal mode Mode { get => mode; set => mode = value; }
+
+        internal StringBuilder Calculate(string str, mode mode)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+            byte[] result;
+            switch (mode)
+            {
+                case mode.md5:
+                    MD5CryptoServiceProvider mD5Crypto = new MD5CryptoServiceProvider();
+                    result = mD5Crypto.ComputeHash(bytes);
+                    break;
+                case mode.sha1:
+                    SHA1CryptoServiceProvider sHA1Crypto = new SHA1CryptoServiceProvider();
+                    result = sHA1Crypto.ComputeHash(bytes);
+                    break;
+                case mode.sha256:
+                    SHA256CryptoServiceProvider sHA256Crypto = new SHA256CryptoServiceProvider();
+                    result = sHA256Crypto.ComputeHash(bytes);
+                    break;
+                case mode.sha384:
+                    SHA384CryptoServiceProvider sHA384Crypto = new SHA384CryptoServiceProvider();
+                    result = sHA384Crypto.ComputeHash(bytes);
+                    break;
+                default:
+                    SHA512CryptoServiceProvider sHA512Crypto = new SHA512CryptoServiceProvider();
+                    result = sHA512Crypto.ComputeHash(bytes);
+                    break;
+            }
+            StringBuilder builder = new StringBuilder();
+            foreach (var a in result)
+                builder.Append(a.ToString("X"));
+            return builder;
+        }
+
+        internal StringBuilder Calculate(FileStream bytes, mode mode)
+        {
+            byte[] result;
+            switch (mode)
+            {
+                case mode.md5:
+                    MD5CryptoServiceProvider mD5Crypto = new MD5CryptoServiceProvider();
+                    result = mD5Crypto.ComputeHash(bytes);
+                    break;
+                case mode.sha1:
+                    SHA1CryptoServiceProvider sHA1Crypto = new SHA1CryptoServiceProvider();
+                    result = sHA1Crypto.ComputeHash(bytes);
+                    break;
+                case mode.sha256:
+                    SHA256CryptoServiceProvider sHA256Crypto = new SHA256CryptoServiceProvider();
+                    result = sHA256Crypto.ComputeHash(bytes);
+                    break;
+                case mode.sha384:
+                    SHA384CryptoServiceProvider sHA384Crypto = new SHA384CryptoServiceProvider();
+                    result = sHA384Crypto.ComputeHash(bytes);
+                    break;
+                default:
+                    SHA512CryptoServiceProvider sHA512Crypto = new SHA512CryptoServiceProvider();
+                    result = sHA512Crypto.ComputeHash(bytes);
+                    break;
+            }
+            StringBuilder builder = new StringBuilder();
+            foreach (var a in result)
+                builder.Append(a.ToString("X"));
+            return builder;
+        }
+    }
+}
---
+++ Hash/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Hash
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private mode mode;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = combo.SelectedIndex;
+            switch(index)
+            {
+                case 0:
+                    mode = mode.md5;
+                    break;
+                case 1:
+                    mode = mode.sha1;
+                    break;
+                case 2:
+                    mode = mode.sha256;
+                    break;
+                case 3:
+                    mode = mode.sha384;
+                    break;
+                case 4:
+                    mode = mode.sha512;
+                    break;
+            }
+
+        }
+
+        private void inputBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Hasher hasher = new Hasher();
+            if(fromText != null && fromText.IsChecked != false)
+            {
+                resultBox.Text = hasher.Calculate(inputBox.Text, mode).ToString();
+            }
+        }
+
+        private void fromText_Checked(object sender, RoutedEventArgs e)
+        {
+            if(button != null)
+                button.IsEnabled = false;
+        }
+
+        private void fromFile_Checked(object sender, RoutedEventArgs e)
+        {
+            if (button != null)
+                button.IsEnabled = true;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            FileInfo info = new FileInfo(inputBox.Text);
+            if(!info.Exists)
+            {
+                resultBox.Text = "文件不存在";
+                return;
+            }
+            else
+            {
+                FileStream fileStream = info.Open(FileMode.Open);
+                Hasher hasher = new Hasher();
+                resultBox.Text = hasher.Calculate(fileStream, mode).ToString();
+            }
+        }
+    }
+}
